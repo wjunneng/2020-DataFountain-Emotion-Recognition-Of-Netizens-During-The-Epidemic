0cca0c9e48bae549ea3c9f1a5a28ade908a9c93b
wwwjjjnnn <1194348056@qq.com>
2020-03-04 13:48:27 +0800

    pytorch_transformers.

--- README.md
+++ README.md
@@ -8,3 +8,11 @@
 > 出现：nCov_10k_test.csv: Non-ISO extended-ASCII text, with very long lines, with CRLF, NEL line terminators
 > 在window系统下,使用记事本打开,点击另存为utf-8格式.
 >>>
+
+
+
+### AEN_BERT:
+
+>>> 1-fold socre: 0.61
+>
+>>> 5-fold socre: 0.63
---
+++ eda/args.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+import os
+import sys
+
+os.chdir(sys.path[0])
+
+# -* orignal dir/path *-
+# project_dir = os.path.dirname(os.path.abspath('..'))
+project_dir = '/home/wjunneng/Ubuntu/2020-DataFountain-Emotion-Recognition-Of-Netizens-During-The-Epidemic'
+data_dir = os.path.join(project_dir, 'data')
+input_dir = os.path.join(data_dir, 'input')
+test_dataset_dir = os.path.join(input_dir, 'test_dataset')
+train_dataset_dir = os.path.join(input_dir, 'train_dataset')
+pretain_model_dir = os.path.join(data_dir, 'pretain_model')
+
+# -* test *-
+test_10k_path = os.path.join(test_dataset_dir, 'nCov_10k_test_utf_8.csv')
+train_100k_path = os.path.join(train_dataset_dir, 'nCoV_100k_train_utf_8.labled.csv')
+train_900k_path = os.path.join(train_dataset_dir, 'nCoV_900k_train_utf_8.unlabled.csv')
+
+# -* generate dir/path *-
+input_categories = '微博中文内容'
+target_categories = '微博发布时间'
+output_categories = '情感倾向'
+
+# -* output *-
+eda_dir = os.path.join(project_dir, 'eda')
+eda_report_path = os.path.join(eda_dir, 'report.txt')
+eda_data_dir = os.path.join(eda_dir, 'data')
+eda_data_train_10k_path = os.path.join(eda_data_dir, 'nCoV_100k_train_utf_8.labled.csv')
+eda_data_test_10k_path = os.path.join(eda_data_dir, 'nCov_10k_test_utf_8.csv')
